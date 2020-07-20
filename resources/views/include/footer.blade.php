b47d3d705621441ad4f4d35e405bfd91ac871702
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-07-20 19:54:21 +0600

    Some Changes in footer and Update readme file.

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,8 +2,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      <a href="https://www.facebook.com/sahaksanjoy/">Developed By <strong>Sanjoy Kumar Saha</strong></a>
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2020
 </footer>
